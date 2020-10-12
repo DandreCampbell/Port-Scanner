597174455833905cd41ea740f30f041d370e9572
Dandre Campbell <dandrecampbell5@gmail.com>
2020-10-12 11:09:40 -0400

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -1,51 +1,60 @@
 #This program takes the URL inputed by the user and returns the Domain Name, IP Address, and any Open Ports
 
-from tld import get_tld
-import os
+from tld import get_tld, get_fld
+import socket
+import sys
 
 """
-This method uses the URL given and returns the Domain Name
+This method uses the URL given and returns the Top Domain Name
 """
 def get_domain_name(url):
-    domain_name = get_tld(url)
-    return domain_name
-
+    domain_name = get_tld(url, as_object=True)
+    return domain_name.fld
 
 """
 This method uses the URL given and returns the first IP Address found
 """
-def get_ip_address(url):
-    commmand = "host " + url
-    data = os.popen(command)
-    results = str(data.read())
-    temp = results.find("has address") + 12
-    ip_address = results[temp:].splitlines()[0]
+def get_ip_address(host):
+    ip_address = socket.gethostbyname(host)
     return ip_address
-
 
 """
 This method uses the IP Addresses of the given URL and returns the Open Ports
 """
 def port_scan(ip):
-    command = "nmap " + ip
-    scan = os.popen(command)
-    ports = str(scan.read())
-    return ports
+    try:
+        for port in range(1,6000):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((ip, port))
+            if result == 0:
+                print("Port {}: 	 Open".format(port))
+            sock.close()
 
+    except socket.gaierror:
+        print("Host could not be identified." + "\n" + "Ending program..." + "\n")
+
+    except KeyboardInterrupt:
+        print("You canceled the scan" + "\n")
+        sys.exit()
+
+    except socket.error:
+        print("Couldn't connect to the host." + "\n")
+        sys.exit()
 
 """
 This main method will print the information created by the methods
 """
 def main():
     user_input = input("Enter a url: ")
+
     domain = get_domain_name(user_input)
-    ip = get_ip_address(user_input)
-    ports = port_scan(get_ip_address(user_input))
+    ip = get_ip_address(domain)
+    ports = port_scan(get_ip_address(ip))
 
     print("URL: " + website + " \n" +
           "Domain Name: " + domain + " \n" +
           "IP Address: " + ip + " \n" +
-          "Open Ports: " + ports + " \n")
+          "Scanning Ports: ... " + ports + " \n")
 
 #Calls the main method and runs the program
 main()
